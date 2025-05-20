ab9e0ea7a73538599790db2050ea87bfbf2bc8f5
federicapaglia96 <federicapaglia96@gmail.com>
2025-05-20 20:22:22 +0200

    hobbies

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-import { createNames } from "./js/function.js";
+import { createNames } from "./js/names.js";
 
 let nomeCognome=createNames("federica","paglia")
 console.log(nomeCognome)
 nomeCognome=createNames("Gianni","Morandi")
 console.log(nomeCognome)
+
+///
+import { hobbies } from "./js/hobbies.js";
+const hobbiesElem=hobbies("bricolage","fotografia","arrampicata")
+console.log(hobbiesElem)
---
+++ js/hobbies.js
@@ -0,0 +1,8 @@
+function hobbies(hobbyOne, hobbyTwo, hobbyThree){
+    const hobbiesArr=[hobbyOne,hobbyTwo,hobbyThree]
+    return hobbiesArr
+}
+
+export{hobbies};
+
+
